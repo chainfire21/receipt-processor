0b5dbcda606d52acdc4031e3499b6e019ddf77f0
Federico Paredes <paredfed@gmail.com>
2024-09-16 15:38:39 -0600

    point logic done!

--- service/service.go
+++ service/service.go
@@ -3,7 +3,11 @@ package service
 import (
 	"encoding/json"
 	"io"
+	"math"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -11,17 +15,17 @@ import (
 
 // Incoming JSON struct that needs to be processed to award points
 type ReceiptToProcess struct {
-	Retailer     string  `json:"retailer"`
-	PurchaseDate string  `json:"purchaseDate"`
-	PurchaseTime string  `json:"purchaseTime"`
-	Items        []Item  `json:"items"`
-	Total        float32 `json:"total"`
+	Retailer     string `json:"retailer"`
+	PurchaseDate string `json:"purchaseDate"`
+	PurchaseTime string `json:"purchaseTime"`
+	Items        []Item `json:"items"`
+	Total        string `json:"total"`
 }
 
 // Items within the JSON struct
 type Item struct {
-	ShortDescription string  `json:"shortDescription"`
-	Price            float32 `json:"price"`
+	ShortDescription string `json:"shortDescription"`
+	Price            string `json:"price"`
 }
 
 // Response struct of GetPoints endpoint
@@ -57,12 +61,16 @@ func HandleProcessReceipts(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(err.Error()))
 	}
 
-	// gotta add point logic now
-
-	newID := uuid.New().String()
-	receipts = append(receipts, Receipt{ID: newID, Points: 55})
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(ProcessReceiptsResponse{ID: newID})
+	points, err := assignPoints(recToProc)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+	} else {
+		newID := uuid.New().String()
+		receipts = append(receipts, Receipt{ID: newID, Points: points})
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(ProcessReceiptsResponse{ID: newID})
+	}
 }
 
 // HandleGetPoints will receive the receipt ID from the URL and returns a JSON object with how many points have been awarded to said receipt
@@ -81,6 +89,77 @@ func HandleGetPoints(w http.ResponseWriter, r *http.Request) {
 	}
 	if !found {
 		w.WriteHeader(404)
-		w.Write([]byte("That ID was not found"))
+		w.Write([]byte("\nThat ID was not found\n"))
 	}
+}
+
+func assignPoints(receipt ReceiptToProcess) (int32, error) {
+	var pointTotal int32
+	pointTotal = 0
+	// Points for retailer alphanumeric amt
+	for _, byte := range []byte(receipt.Retailer) {
+		if isAlphaNumeric(byte) {
+			pointTotal++
+		}
+	}
+
+	// Points for if total is a round number
+	totalFloat, err := strconv.ParseFloat(receipt.Total, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	if totalFloat == math.Trunc(totalFloat) {
+		pointTotal += 50
+	}
+
+	// Points if total is multiple of 0.25
+	if math.Remainder(totalFloat, 0.25) == 0 {
+		pointTotal += 25
+	}
+
+	// Points for every pair of items on receipt
+	pointTotal += int32(len(receipt.Items)/2) * 5
+
+	// Points for multiple of 3 item description
+	for _, item := range receipt.Items {
+		trimmedStr := strings.Trim(item.ShortDescription, " ")
+		if math.Remainder(float64(len(trimmedStr)), 3) == 0 {
+			// Multiply price by 0.2 and round up to nearest integer
+			priceFloat, err := strconv.ParseFloat(item.Price, 64)
+			if err != nil {
+				return 0, err
+			}
+			pointTotal += int32(math.Trunc(priceFloat*0.2)) + 1
+		}
+	}
+
+	// Points for odd purchase day
+	date, err := time.Parse(time.DateOnly, receipt.PurchaseDate)
+	if err != nil {
+		return 0, err
+	}
+
+	if math.Remainder(float64(date.Day()), 2) != 0 {
+		pointTotal += 6
+	}
+
+	// Points for time of purchase between 2 and 4 pm
+	receiptTime, err := time.Parse(time.TimeOnly, receipt.PurchaseTime+":00")
+	if err != nil {
+		return 0, err
+	}
+	afterTime, _ := time.Parse(time.TimeOnly, "14:00:00")
+	beforeTime, _ := time.Parse(time.TimeOnly, "16:00:00")
+
+	if receiptTime.After(afterTime) && receiptTime.Before(beforeTime) {
+		pointTotal += 10
+	}
+
+	return pointTotal, nil
+}
+
+func isAlphaNumeric(c byte) bool {
+	// Check if the byte value falls within the range of alphanumeric characters
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')
 }
